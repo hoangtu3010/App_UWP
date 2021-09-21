ccc5710b483c12b082e43e3894d072b77d681453
hoang <hoangtu2378@gmail.com>
2021-09-22 05:40:13 +0700

    tu-up

--- AppUWP/MainPage.xaml.cs
+++ AppUWP/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using AppUWP.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,41 @@ namespace AppUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static Frame _frame;
+
         public MainPage()
         {
             this.InitializeComponent();
+            MainFrame.Navigate(typeof(Pages.Home));
+        }
+
+        private void ListView_Loaded(object sender, RoutedEventArgs e)
+        {
+            var item = new MenuModel() { Name = "Home", NamePage = "home", Icon = "/Assets/button1.png" };
+            var item2 = new MenuModel() { Name = "Eat-in", NamePage = "eat-in", Icon = "/Assets/button2.png" };
+            var item3 = new MenuModel() { Name = "Collection", NamePage = "collection", Icon = "/Assets/button3.png" };
+            var item4 = new MenuModel() { Name = "Delivery", NamePage = "delivery", Icon = "/Assets/button4.png" };
+            var item5 = new MenuModel() { Name = "Take Away", NamePage = "take-away", Icon = "/Assets/button5.png" };
+            var item6 = new MenuModel() { Name = "Driver Payment", NamePage = "driver-payment", Icon = "/Assets/button6.png" };
+            var item7 = new MenuModel() { Name = "Customers", NamePage = "customers", Icon = "/Assets/button7.png" };
+
+            Menu.Items.Add(item);
+            Menu.Items.Add(item2);
+            Menu.Items.Add(item3);
+            Menu.Items.Add(item4);
+            Menu.Items.Add(item5);
+            Menu.Items.Add(item6);
+            Menu.Items.Add(item7);
+        }
+
+        private void ListViewItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MenuModel selectedItem = (MenuModel)Menu.SelectedItem;
+            switch (selectedItem.NamePage)
+            {
+                case "home": MainFrame.Navigate(typeof(Pages.Home), selectedItem); break;
+                case "eat-in": MainFrame.Navigate(typeof(Pages.Eat_in)); break;
+            }
         }
     }
 }
---
+++ AppUWP/Pages/Eat-in.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using AppUWP.Models;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AppUWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Eat_in : Page
+    {
+        public Eat_in()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Gv_Loaded(object sender, RoutedEventArgs e)
+        {
+            var item = new ProductModel() { Name = "Burger", Image = "/Assets/picture-box-1.jpg", Description = "A hamburger is a food, typically considered a sandwich", Price = "$13" };
+            var item2 = new ProductModel() { Name = "Cake", Image = "/Assets/picture-box-2.jpg", Description = "A cake is a food, typically considered a sandwich.......,,,,,,,", Price = "$23" };
+            var item3 = new ProductModel() { Name = "Pizza", Image = "/Assets/picture-box-3.jpg", Description = "A Pizza is a food, typically considered a sandwich.......,,,,,,,", Price = "$45" };
+
+
+            Gv.Items.Add(item);
+            Gv.Items.Add(item2);
+            Gv.Items.Add(item3);
+
+        }
+    }
+}
