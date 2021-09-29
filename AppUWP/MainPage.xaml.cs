376bf2515fa249bcc277aa81dbf1e9370c147a4d
hoang <hoangtu2378@gmail.com>
2021-09-29 10:08:03 +0700

    up-tu

--- AppUWP/MainPage.xaml.cs
+++ AppUWP/MainPage.xaml.cs
@@ -29,7 +29,7 @@ namespace AppUWP
         {
             this.InitializeComponent();
             _frame = MainFrame;
-            MainFrame.Navigate(typeof(Pages.Home));
+            MainFrame.Navigate(typeof(Pages.Cart));
         }
 
         private void ListView_Loaded(object sender, RoutedEventArgs e)
